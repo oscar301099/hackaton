39bc50498dfe1931275faa4e4774520a141a3e1d
oscar301099 <oscarorosduran@gmail.com>
2024-08-11 03:41:27 -0400

    change deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
+    solidity: "0.8.7",
+    networks: {
+      avalanche: {
+        url: "https://api.avax.network/ext/bc/C/rpc",
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      },
+      etherscan: {
+        apiKey: process.env.AVALANCHESCAN_API_KEY
+      },
     tailwindcss: {},
     autoprefixer: {},
   },
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,24 @@
-const hre = require('hardhat')
-const fs = require('fs')
+const hre = require('hardhat');
+const fs = require('fs');
 
 async function main() {
-  const taxFee = 5
-  const Contract = await hre.ethers.getContractFactory('Genesis')
-  const contract = await Contract.deploy(taxFee)
+  const taxFee = 5;
+  const Contract = await hre.ethers.getContractFactory('Genesis');
+  const contract = await Contract.deploy(taxFee);
 
-  await contract.deployed()
+  await contract.deployed();
 
-  const address = JSON.stringify({ address: contract.address }, null, 4)
+  const address = JSON.stringify({ address: contract.address }, null, 4);
   fs.writeFile('./src/abis/contractAddress.json', address, 'utf8', (err) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-    console.log('Deployed contract address', contract.address)
-  })
+    console.log('Deployed contract address', contract.address);
+  });
 }
 
 main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
-})
+  console.error(error);
+  process.exitCode = 1;
+});
